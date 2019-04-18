55e8e4fae96d6e69b47f154de428f04d3eeb710b
Juan Padilla <padilla.juan@solute.us>
2019-04-18 11:32:46 -0700

    return json data

--- app/src/main/java/com/example/dexweather/MainActivity.java
+++ app/src/main/java/com/example/dexweather/MainActivity.java
@@ -1,16 +1,33 @@
 package com.example.dexweather;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
     // API KEY: 0b3a33b127b30991aad586510ac20441
-    // Sample API call:
     // https://api.darksky.net/forecast/0b3a33b127b30991aad586510ac20441/37.8267,-122.4233
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String apiKey = "0b3a33b127b30991aad586510ac20441";
+        double latitude = 32.7157;
+        double longitude = -117.1611;
+        String url = "https://api.darksky.net/forecast/"
+                + apiKey + "/"
+                + String.valueOf(latitude) + ","
+                + String.valueOf(longitude);
+        try {
+            String jsonGetResult = new WeatherModel().execute(url).get();
+            System.out.println(jsonGetResult);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ app/src/main/java/com/example/dexweather/WeatherModel.kt
@@ -0,0 +1,40 @@
+package com.example.dexweather
+
+import android.os.AsyncTask
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.io.Reader
+import java.net.HttpURLConnection
+import java.net.URL
+
+// Gets JSON data from Dark Sky
+class WeatherModel : AsyncTask<String, Void, String>() {
+    // Returns JSON Data from GET Request
+    override fun doInBackground(vararg query: String): String {
+        var jsonResult = ""
+        var input: String?
+        try {
+            val searchURL = URL(query[0])
+            val httpConnection = searchURL.openConnection() as HttpURLConnection
+            // SEND REQ AND CONNECT
+            httpConnection.requestMethod = "GET";
+            httpConnection.connect()
+            val stream = InputStreamReader(httpConnection.inputStream)
+            val buffReader = BufferedReader(stream as Reader?)
+            val stringBuilder = StringBuilder()
+            input = buffReader.readLine()
+            while (input != null) {
+                stringBuilder.append(input)
+                input = buffReader.readLine()
+            }
+            buffReader.close()
+            stream.close()
+            jsonResult = stringBuilder.toString()
+        } catch (e: IOException) {
+            print("ERROR");
+            e.printStackTrace()
+        }
+        return jsonResult
+    }
+}
