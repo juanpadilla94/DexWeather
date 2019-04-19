867e19a4eddd52c2753b6a5a5e44f6143c726f4a
Juan Padilla <padilla.juan@solute.us>
2019-04-19 10:56:03 -0700

    add refresh button

--- app/src/main/java/com/example/dexweather/MainActivity.java
+++ app/src/main/java/com/example/dexweather/MainActivity.java
@@ -1,26 +1,26 @@
 package com.example.dexweather;
 
 import android.graphics.Color;
-import android.support.constraint.solver.widgets.Rectangle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
-import android.view.ViewGroup;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
-    boolean colorFlip;
+    private String apiKey;
+    public double latitude;
+    public double longitude;
+    private String url;
+    private boolean colorFlip;
 
     // API KEY: 0b3a33b127b30991aad586510ac20441
     // https://api.darksky.net/forecast/0b3a33b127b30991aad586510ac20441/37.8267,-122.4233
@@ -28,17 +28,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getWeather();
-    }
-
-    private void getWeather() {
-        String apiKey = "0b3a33b127b30991aad586510ac20441";
-        double latitude = 32.7157;
-        double longitude = -117.1611;
-        String url = "https://api.darksky.net/forecast/"
+        apiKey = "0b3a33b127b30991aad586510ac20441";
+        latitude = 32.7157;
+        longitude = -117.1611;
+        url = "https://api.darksky.net/forecast/"
                 + apiKey + "/"
                 + latitude + ","
                 + longitude;
+        getWeather();
+        Button refreshButton = (Button)findViewById(R.id.refresh_button);
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout linearLay = findViewById(R.id.linearLayout);
+                linearLay.removeAllViews();
+                getWeather();
+                Toast.makeText(MainActivity.this,
+                        "Page Refreshed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void getWeather() {
         try {
             String jsonGetResult = new WeatherModel().execute(url).get();
             String daily = new JSONObject(jsonGetResult).get("daily").toString();
@@ -63,20 +74,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void displayWeather(String summary, String highTemp, String lowTemp) {
-        CardView dayCard = new CardView(this);
-        dayCard.setMinimumHeight(300);
+        CardView weatherCard = new CardView(this);
+        weatherCard.setMinimumHeight(300);
         TextView cardInfo = new TextView(this);
         cardInfo.setText("\n" + summary + "\n" + "high : " + highTemp + "\n" + "low : " + lowTemp);
-        dayCard.addView(cardInfo);
+        weatherCard.addView(cardInfo);
         if(colorFlip = !colorFlip) { // Changes color of card every other time
-            dayCard.setCardBackgroundColor(Color.parseColor("#A374FF"));
+            weatherCard.setCardBackgroundColor(Color.parseColor("#A374FF"));
             cardInfo.setTextColor(Color.parseColor("#FFFFFF"));
         }
         else {
-            dayCard.setCardBackgroundColor(Color.parseColor("#ffffff"));
+            weatherCard.setCardBackgroundColor(Color.parseColor("#ffffff"));
             cardInfo.setTextColor(Color.parseColor("#000000"));
         }
+        weatherCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TO DO: Add view with extra data
+            }
+        });
         LinearLayout linearLay = findViewById(R.id.linearLayout);
-        linearLay.addView(dayCard);
+        linearLay.addView(weatherCard);
     }
 }
